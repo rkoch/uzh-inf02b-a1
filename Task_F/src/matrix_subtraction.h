6572ed25f4e4fb32411cc66fd13ac9eabee8a807
Remo Koch <me@rko.io>
2013-02-24 13:12:08 +0100

    Finished Task F

---
+++ Task_F/src/matrix_subtraction.h
@@ -0,0 +1,15 @@
+/*
+ *  matrix_subtraction.h
+ *
+ *  Created by Radhika Garg on 14/03/12.
+ *  Copyright 2012 University of Zurich. All rights reserved.
+ *
+ *  Authored by Remo Koch
+ *
+ */
+
+void initialiseMemory(int **&x, int row, int col);
+void fill(int **&x, int row, int col);
+int **subtractMatrices(int **x, int **y, int row, int col);
+void deallocateMemory(int **&val, int x, int y);
+void display(int **val, int x, int y);
